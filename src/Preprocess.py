f1638bdd1821d467758b36181ae2d78c6b378c88
Lab Share <labshare@mwgk6dh9j9kt.med.yale.internal>
2023-12-14 15:33:32 -0500

    scprep, jupyter

---
+++ src/Preprocess.py
@@ -0,0 +1,15 @@
+
+# --------------------------------------------------
+# Preprocess.py
+# Marcello DiStasio
+# Dec 2023
+# Uses scprep and PHATE etc.
+
+
+import os
+
+FILEPATHBASE = '..'
+
+SAVEFIGS = True
+if SAVEFIGS:
+    IMGDIR = os.path.join(FILEPATHBASE,'img/')
